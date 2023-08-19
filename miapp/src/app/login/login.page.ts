4f3eacaf3fa7053c6cfc228d3f31cd7c94e4f3db
nixtoon <127158636+nixtoon@users.noreply.github.com>
2023-08-18 20:27:11 -0400

    modulos node

--- miapp/src/app/login/login.page.ts
+++ miapp/src/app/login/login.page.ts
@@ -1,21 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { Router, NavigationExtras } from '@angular/router';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
+export class LoginPage {
+  usuario: string = '';
+  password: string = '';
 
+  constructor(private router: Router) {}
 
-export class LoginPage implements OnInit {
-
-  constructor(private router: Router) { }
-  navigate(){
-    this.router.navigate(['/home'])
+  agregarUsuario() {
+    if (this.usuario.length >= 3 && this.usuario.length <= 8 && /^\d{4}$/.test(this.password)) {
+      this.router.navigate(['/home'], {
+        queryParams: {
+          usuario: this.usuario,
+          password: this.password,
+        },
+      });
+    } else {
+      // Mostrar mensaje de error
+    }
   }
-
-  ngOnInit() {
-  }
-
 }
