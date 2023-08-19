9903904c99eb1810d11fef2d14c63fc7a6861b01
nixtoon <127158636+nixtoon@users.noreply.github.com>
2023-08-18 21:09:36 -0400

    boton arreglado

--- miapp/src/app/login/login.page.ts
+++ miapp/src/app/login/login.page.ts
@@ -7,21 +7,10 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.page.scss'],
 })
 export class LoginPage {
+  
   usuario: string = '';
   password: string = '';
 
   constructor(private router: Router) {}
 
-  agregarUsuario() {
-    if (this.usuario.length >= 3 && this.usuario.length <= 8 && /^\d{4}$/.test(this.password)) {
-      this.router.navigate(['/home'], {
-        queryParams: {
-          usuario: this.usuario,
-          password: this.password,
-        },
-      });
-    } else {
-      // Mostrar mensaje de error
-    }
-  }
 }
